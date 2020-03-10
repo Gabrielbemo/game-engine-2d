1ffcbee24a8c1673fd30cd5adc95dfee2cb033b8
Gabrielbemo <gabrielbemoura@gmail.com>
2020-03-10 01:53:34 -0300

    adicionando um game looping

--- src/br/com/engine/Game.java
+++ src/br/com/engine/Game.java
@@ -6,12 +6,18 @@ import java.awt.*;
 public class Game extends Canvas implements Runnable {
 
     public static JFrame frame;
+    private Thread thread;
+    private boolean isRunning = true;
     private final int WIDTH = 160;
     private final int HEIGHT = 120;
     private final int SCALE = 4;
 
     public Game() {
         setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        initFrame();
+    }
+
+    public void initFrame() {
         frame = new JFrame("Main screen");
         frame.add(this);
         frame.setResizable(false);
@@ -21,11 +27,24 @@ public class Game extends Canvas implements Runnable {
         frame.setVisible(true);
     }
 
+    public synchronized void start() {
+        thread = new Thread(this);
+        isRunning = true;
+        thread.start();
+    }
+
+    public synchronized void stop() {
+
+    }
+
     public static void main(String[] args) {
         Game game = new Game();
+        game.start();
     }
 
     public void run() {
-
+        while (isRunning) {
+            System.out.println("Game rodando");
+        }
     }
 }
