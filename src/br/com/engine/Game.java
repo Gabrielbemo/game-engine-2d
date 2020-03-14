7e1caf95fd03c77e9356f16177c854a671478238
Gabrielbemo <gabrielbemoura@gmail.com>
2020-03-14 17:12:00 -0300

    melhorando o game looping

--- src/br/com/engine/Game.java
+++ src/br/com/engine/Game.java
@@ -42,9 +42,37 @@ public class Game extends Canvas implements Runnable {
         game.start();
     }
 
+    public void tick() {
+
+    }
+
+    public void render() {
+
+    }
+
     public void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        int frames = 0;
+        double timer = System.currentTimeMillis();
         while (isRunning) {
-            System.out.println("Game rodando");
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            if (delta >= 1) {
+                tick();
+                render();
+                frames++;
+                delta--;
+            }
+
+            if(System.currentTimeMillis() - timer >= 1000){
+                System.out.println("fps: " + frames);
+                frames = 0;
+                timer = System.currentTimeMillis();
+            }
         }
     }
 }
